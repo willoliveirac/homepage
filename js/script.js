9fee30503631dba78b50ed3c40f28a6376aba247
Will Oliveira <will.oliveirac@gmail.com>
2023-04-05 04:35:20 -0300

    Adcionado links sociais e form de envio de email

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+function sendEmail() {
+
+
+    Email.send({
+            
+        SecureToken: "447669aa-8db4-473f-b790-0f9e933d79d7",
+        To: 'will.oliveirac@gmail.com',
+        From: "visitantesite27@gmail.com",
+        Subject: document.getElementById("Assunto").value,
+        Body: "Nome: "+ document.getElementById("nome").value + " Email: "+document.getElementById("email").value+
+        "\n" + "Mensagem: \n" + document.getElementById("message").value
+    }).then(
+        message => alert(message)
+    );
+
+}
